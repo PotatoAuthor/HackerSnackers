f6576a30631710a53abed262ba200c05ef3a90e9
Simon Dick <simon.dick@mail.utoronto.ca>
2022-02-05 17:25:11 -0500

    implement fixed length array

--- main.py
+++ main.py
@@ -26,18 +26,21 @@ SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 # Returns: the schedule array
 def get_free_time(cals_to_check, service, calendars_dict, max_scan_time_days):
         #fixed size array filled with 0s, which has the number of 15 minute time blocks in max_scan_time_days
-        schedule_array = [0]*(max_scan_time_days*96) 
+        # minus one since the first bucket starts at the NEXT quarter hour
+        schedule_array = [0]*(max_scan_time_days*96-1) 
+        
         # gets current timestamp
         now = str(datetime.datetime.now(datetime.timezone.utc).isoformat()[:-6] + 'Z')  # 'Z' indicates UTC time
-
         # gets timestamp of the end of the serach range (in UTC)
-        max_search_timestamp = str((datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(days = max_scan_time_days)).isoformat()[:-6] + 'Z')
+        max_search_timestamp = str((datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(days = max_scan_time_days, minutes = 15)).isoformat()[:-6] + 'Z')
 
-        # this convoluted line of code finds the exact timestamp of the next [LOCAL TIME]
+        # this convoluted line of code finds the exact timestamp of the next quarter of an hour[LOCAL TIME]
         first_bucket_time = (datetime.datetime.now() + datetime.timedelta(minutes = 15-(datetime.datetime.now().minute % 15))).replace(microsecond=0, second = 0)
 
+        # loop through each calendar that was selected
         for calendarName in cals_to_check:
             calId=calendars_dict[calendarName]
+
             # use page iteration to loop over all events between 2 time points. At the earliest, they must already be in progress at execution
             # At the latest, they must start before max_search_timestamp
             page_token = None
@@ -45,23 +48,28 @@ def get_free_time(cals_to_check, service, calendars_dict, max_scan_time_days):
                 events = service.events().list(calendarId=calId, timeMin=now,
                                               timeMax=max_search_timestamp, singleEvents=True).execute()
                 for event in events['items']:
+                    # find the difference between the end time and the first bucket in the array
                     end_time = datetime.datetime.strptime(event['end']['dateTime'].replace('T', ' ')[:-6], '%Y-%m-%d %H:%M:%S') # [LOCAL TIME]
                     timediff = end_time - first_bucket_time # result is a timedelta object
                     minutesOffset = timediff.days*1440 + timediff.seconds//60
-
                     # index event_ending_index is the last index in the array which should be set to 'occupied'
                     event_ending_index = int(minutesOffset/15 -1)
 
+                    # find the difference between the start time and the first bucket in the array
                     start_time = datetime.datetime.strptime(event['start']['dateTime'].replace('T', ' ')[:-6], '%Y-%m-%d %H:%M:%S') # [LOCAL TIME]
                     timediff = start_time - first_bucket_time # result is a timedelta object
                     minutesOffset = timediff.days*1440 + timediff.seconds//60
                     event_start_index = int(minutesOffset/15)
+
+                    # fill the schedule array with 1's when the event is taking place
                     if (event_ending_index >= 0):
                         schedule_array[max(0, event_start_index):event_ending_index] = [1]*(event_ending_index-max(0, event_start_index)+1)
+                        
                 page_token = events.get('nextPageToken')
                 if not page_token:
                     break
-        return schedule_array
+        # use this to make python have a fixed length array
+        return schedule_array[0:max_scan_time_days*96-1]
 
 def main():
     """Shows basic usage of the Google Calendar API.
@@ -101,13 +109,10 @@ def main():
                 break
         
         # ask the user to check boxes indicating which calendars to analyze
-        for key in calendars_dict:
-            print(key, ' : ', calendars_dict[key])
-
         cals_to_check = ['golem8gamer@gmail.com', 'Holidays in Canada', 'broland']
-        max_scan_time_days = 1
-        free_time = get_free_time(cals_to_check, service, calendars_dict, max_scan_time_days)
-        
+        max_scan_time_days = 3
+        free_time_array = get_free_time(cals_to_check, service, calendars_dict, max_scan_time_days)
+        print (free_time_array)
 
 
     except HttpError as error:
