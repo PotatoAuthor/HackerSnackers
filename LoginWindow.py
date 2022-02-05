2e4a9a7cfedb87c5f13ad7bced3847d009456533
Thomas <thomas.liu1303@gmail.com>
2022-02-05 17:43:25 -0500

    Added call to main interface

--- LoginWindow.py
+++ LoginWindow.py
@@ -1,59 +1,56 @@
 import pygame
 from pygame import MOUSEBUTTONDOWN
 import LoginActivity
+import gui
 
-pygame.init()
 
-# width and height of window
-width, height = pygame.display.Info().current_w, pygame.display.Info().current_h
+def main():
+    """Main function of the """
+    pygame.init()
 
-# text and button positions
-text_x, text_y = 10, 10
-button_x, button_y = 30, 80
+    # width and height of window
+    width, height = pygame.display.Info().current_w, pygame.display.Info().current_h
 
-# pygame.key.set_repeat(300, 30)
-screen = pygame.display.set_mode((width//4, height//4))
-screen.fill((255, 255, 255))
+    # text and button positions
+    text_x, text_y = 10, 10
+    button_x, button_y = 30, 80
 
-# set icon and window title
-logo = pygame.image.load("Assets/Amitee_Logo.png")
-pygame.display.set_icon(logo)
-pygame.display.set_caption("Amitee")
+    # pygame.key.set_repeat(300, 30)
+    screen = pygame.display.set_mode((width // 4, height // 4))
+    screen.fill((255, 255, 255))
 
-# collision rectangle for button
-rect = pygame.Rect(button_x, button_y, 412, 96)
+    # set icon and window title
+    logo = pygame.image.load("Assets/Amitee_Logo.png")
+    pygame.display.set_icon(logo)
+    pygame.display.set_caption("Amitee")
 
-# button image
-loginBox = pygame.image.load("Assets/btn_google_sign-in.png")
-screen.blit(loginBox, (button_x, button_y))
+    # collision rectangle for button
+    rect = pygame.Rect(button_x, button_y, 412, 96)
 
-# buttonSurface = pygame.display.set_mode(())
+    # button image
+    loginBox = pygame.image.load("Assets/btn_google_sign-in.png")
+    screen.blit(loginBox, (button_x, button_y))
 
-# pygame.draw.rect(screen, (255,0,0), rect)
+    pygame.display.flip()
 
-pygame.display.flip()
+    clock = pygame.time.Clock()
+    playing_game = True
+    while playing_game:
+        clock.tick(45)
 
-# #when left arrow is pressed, the red rect goes to the left
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                playing_game = False
+                break
+            if event.type == MOUSEBUTTONDOWN:
+                mouse_pos = event.pos
+                if rect.collidepoint(mouse_pos):
+                    LoginActivity.get_oauth2_token()
+                    pygame.quit()
+                    gui.main()
 
-clock = pygame.time.Clock()
-playing_game = True
-while playing_game:
-    clock.tick(45)
+    pygame.quit()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            playing_game = False
-            break
-#         elif event.type == pygame.KEYDOWN:
-#             if event.key == pygame.K_LEFT:
-#                 pygame.draw.rect(screen, (255,255,255), rect)
-#                 pygame.display.update(rect)
-#                 rect.move_ip((-5,0))
-#                 pygame.draw.rect(screen, (255,0,0), rect)
-#                 pygame.display.update(rect)
-        if event.type == MOUSEBUTTONDOWN:
-            mouse_pos = event.pos  # Now it will have the coordinates of click point.
-            if rect.collidepoint(mouse_pos):
-                LoginActivity.get_oauth2_token()
 
-pygame.quit()
+if __name__ == '__main__':
+    main()
