47f90b6af2c3977c3de44474b86c66d91b32507c
Boaz <boazcheung2003@gmail.com>
2022-02-05 17:26:40 -0500

    looks better

--- gui.py
+++ gui.py
@@ -8,30 +8,35 @@ for c in calendar_list:
     cal.append(c)
 
 def main():
-    title_element = thorpy.make_text("Chose calendar to use", 22, (0, 0, 0))
+    title_element = thorpy.make_text("Chose calendar to use", 40, (236, 158, 80))
     elements = [title_element]
     if calendar_length >= 1:
         element1 = thorpy.make_button(cal[0], func=a)
+        element1.set_size((150, 50))
         elements.append(element1)
     if calendar_length >= 2:
         element2 = thorpy.make_button(cal[1], func=b)
+        element2.set_size((150, 50))
         elements.append(element2)
     if calendar_length >= 3:
         element3 = thorpy.make_button(cal[2], func=c)
+        element3.set_size((150, 50))
         elements.append(element3)
     if calendar_length >= 4:
         element4 = thorpy.make_button(cal[3], func=d)
+        element4.set_size((150, 50))
         elements.append(element4)
     if calendar_length >= 5:
         element5 = thorpy.make_button(cal[4], func=e)
+        element5.set_size((150, 50))
         elements.append(element5)
     e_quit = thorpy.make_button("Done", func=done)
-    e_quit.set_size((300, 100))
+    e_quit.set_size((350, 50))
+    e_quit.center()  # center the title on the screen
+    e_quit.set_topleft((None, 600))
     elements.append(e_quit)
-    sub_title = thorpy.make_text("You have selected" + str(ans), 15, (0, 0, 0))
-    elements.append(sub_title)
-    background = thorpy.Background(color=(200, 200, 200), elements=elements)
-    thorpy.store(background)
+    background = thorpy.Background(image='using.jpeg', elements=elements)
+    thorpy.store(background, elements[:-1])
     menu = thorpy.Menu(background)
     menu.play()
 
@@ -54,6 +59,7 @@ def done():
     #the set of keys to the dictionary that is selected
     print(ans)
     thorpy.functions.quit_menu_func()
+    thorpy.functions.quit_menu_func()
 
 
 application = thorpy.Application(size=(1000, 800), caption="Amitee")
@@ -61,22 +67,19 @@ application = thorpy.Application(size=(1000, 800), caption="Amitee")
 
 e_title = thorpy.make_text("Amitee", font_size=40, font_color=(10, 10, 10))
 e_title.center()
-e_title.set_topleft((None, 20))
 
 e_subtitle = thorpy.make_text("Restoration of Friendship", font_size=15, font_color=(10, 10, 10))
-e_subtitle.move((400, 80))
 
-
-e_options = thorpy.make_button('start', func=main)
-e_options.set_size((300, 100))
+e_options = thorpy.make_button('Start', func=main)
+e_options.set_size((250, 80))
 
 e_quit = thorpy.make_button("Quit", func=thorpy.functions.quit_menu_func)
-e_quit.set_size((300, 100))
+e_quit.set_size((250, 80))
 
 
-e_background = thorpy.Background(color=(170, 170, 170),
-                                 elements=[e_title, e_subtitle, e_options, e_quit])
-thorpy.store(e_background, [e_options, e_quit])
+
+e_background = thorpy.Background(image='friends.jpeg', elements=[e_title, e_subtitle, e_options, e_quit])
+thorpy.store(e_background)
 
 menu = thorpy.Menu(e_background)
 menu.play()
